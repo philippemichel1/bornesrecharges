51eeda24d404e67e2988a386d994e793b9ef9fa5
Philippe MICHEL <philippe.michel1@gmail.com>
2022-06-06 17:16:22 +0200

    Initial Commit

---
+++ BornesRecharches/BornesRecharchesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BornesRecharchesApp.swift
+//  BornesRecharches
+//
+//  Created by Philippe MICHEL on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BornesRecharchesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
